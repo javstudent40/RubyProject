d193a212102fa072a64194419594433600950804
javstudent40 <titusgr30@yahoo.com>
2019-03-18 18:59:28 -0400

    added delete functionality to application

--- conference/app/controllers/customers_controller.rb
+++ conference/app/controllers/customers_controller.rb
@@ -36,6 +36,12 @@ class CustomersController < ApplicationController
 		end
 	end
 
+	def destroy
+		@customer = Customer.find(params[:id])
+		@customer.destroy
+		redirect_to customers_path
+	end
+
 	private
 
 	def customer_params
